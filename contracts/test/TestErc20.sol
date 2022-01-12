b5a247414b2378b541771a29d5b02566b54db48e
Guillaume Felley <guillaume@felley.io>
2022-01-12 13:44:14 +0100

    nits

--- contracts/test/TestErc20.sol
+++ contracts/test/TestErc20.sol
@@ -2,6 +2,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Test is ERC20("Incoom Token", "INC") {
     constructor() public {
-        _mint(msg.sender, 1e6);
+        _mint(msg.sender, 1e9 ether);
     }
 }
